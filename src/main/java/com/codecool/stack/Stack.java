313b6de69b78dcd9fc824631c274d47cc0715752
johnny-sack1 <jonymaka@gmail.com>
2018-10-09 04:42:26 +0200

    Implement peek() and push()

--- src/main/java/com/codecool/stack/Stack.java
+++ src/main/java/com/codecool/stack/Stack.java
@@ -1,4 +1,19 @@
 package com.codecool.stack;
 
-public class Stack {
+public class Stack<T> {
+    private int size;
+    private T[] stackArray;
+
+    public Stack(int size) {
+        this.size = size;
+        this.stackArray = (T[]) new Object[size];
+    }
+
+    public void push(T t) {
+        stackArray[size - 1] = t;
+    }
+
+    public T peek() {
+        return stackArray[size - 1];
+    }
 }
--- src/test/java/com/codecool/stack/StackTest.java
+++ src/test/java/com/codecool/stack/StackTest.java
@@ -1,4 +1,22 @@
 package com.codecool.stack;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class StackTest {
+    private Stack<String> stack;
+
+    @BeforeEach
+    void setup() {
+        stack = new Stack<>(10);
+    }
+
+    @Test
+    void testPeek() {
+        String item = "test string";
+        stack.push(item);
+        assertEquals(item, stack.peek());
+    }
 }
